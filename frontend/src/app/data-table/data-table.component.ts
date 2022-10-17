c38fa716dc28221f6538d04d7341c3375b4b8272
Chris Ricketts <cricketts497@gmail.com>
2022-10-17 15:01:03 +0100

    Switch to observable-based data source

--- frontend/src/app/data-table/data-table.component.ts
+++ frontend/src/app/data-table/data-table.component.ts
@@ -1,4 +1,6 @@
+import { DataSource } from '@angular/cdk/collections';
 import { Component } from '@angular/core';
+import { Observable, ReplaySubject } from 'rxjs';
 import { DataTableRow } from './data-model';
 
 @Component({
@@ -8,8 +10,36 @@ import { DataTableRow } from './data-model';
 })
 export class DataTableComponent {
   displayedColumns: string[] = ['symbol', 'z', 'n', 'jp', 'magnetic_dipole', 'electric_quadrupole']
-  dataSource: DataTableRow[] = [
-    {z:49, n:66, symbol:'In', jp:'9/2+', magnetic_dipole:'4.123(3)', electric_quadrupole:'2.323'},
-    {z:50, n:66, symbol:'Sn', jp:'5+', magnetic_dipole:'5.234', electric_quadrupole:'0.123(1)'}
-  ];
+  dataSource = new DataTableSource();
+
+  constructor() {
+    this.dataSource.addRow({z:49, n:66, symbol:'In', jp:'9/2+', magnetic_dipole:'4.123(3)', electric_quadrupole:'2.323'});
+    this.dataSource.addRow({z:50, n:66, symbol:'Sn', jp:'5+', magnetic_dipole:'5.234', electric_quadrupole:'0.123(1)'});
+  }
+}
+
+class DataTableSource extends DataSource<DataTableRow> {
+  private dataStream = new ReplaySubject<DataTableRow[]>;
+  private dataTableRows: DataTableRow[];
+
+  constructor(initialRows: DataTableRow[] = []) {
+    super();
+    this.dataTableRows = initialRows;
+    this.update();
+  }
+
+  connect(): Observable<DataTableRow[]> {
+    return this.dataStream;
+  }
+
+  disconnect() {}
+
+  addRow(row: DataTableRow): void {
+    this.dataTableRows.push(row);
+    this.update();
+  }
+
+  private update() {
+    this.dataStream.next(this.dataTableRows);
+  }
 }
